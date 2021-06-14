4c6999295482a25bc2343001c08388c1275dc5f3
Felipe Bomtempo <felipeb.01@outlook.com>
2021-06-14 11:04:17 -0300

    Front-end do menu

---
+++ import/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <div class="container-fluid" style="font-size: 22px">
+        <a href="menu.php"><img src="./imagens/icon-principal.png" width="60" class="navbar-brand"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="menu.php">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Cadastros
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Cadastro de Pacientes</a></li>
+                        <li><a class="dropdown-item" href="#">Cadastro de Médicos</a></li>
+                        <li><a class="dropdown-item" href="#">Cadastro de Funcionários</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <span class="px-4">USUÁRIO: felipe</span>
+            <form class="d-flex">
+                <button class="btn btn-lg btn-outline-dark" type="submit">Logout<i class="fas fa-sign-out-alt" style="margin-left: 0.5vw"></i></button>
+            </form>
+        </div>
+    </div>
+</nav>
---
+++ menu.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="./css/menu.css">
+    <link rel="import" href="./import/navbar.html">
+
+    <title>Menu</title>
+
+</head>
+
+<body>
+
+    <!-- Include Navbar -->
+    <?php include('./import/navbar.php'); ?>
+
+    <h1 class="text-center pt-5 display-3">Bem-vindo(a) de volta, Felipe!</h1>
+    <div class="container" style="padding-top: 9vh">
+        <div class="row">
+            <div class="pb-5 col d-flex justify-content-center">
+                <a href="" class="icon-link">
+                    <div class="card" style="width: 18rem;">
+                        <div class="text-center" style="font-size: 100px"><i class="fas fa-calendar-day text-center"></i></div>
+                        <div class="card-body">
+                            <h5 class="text-center card-title h3">Agendamento</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="pb-5 col d-flex justify-content-center">
+                <a href="" class="icon-link">
+                    <div class="card" style="width: 18rem;">
+                        <div class="text-center" style="font-size: 100px"><i class="fas fa-stethoscope"></i></div>
+                        <div class="card-body">
+                            <h5 class="text-center card-title h3">Atendimento</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="pb-5 col d-flex justify-content-center">
+                <a href="" class="icon-link">
+                    <div class="card" style="width: 18rem;">
+                        <div class="text-center" style="font-size: 100px"><i class="fas fa-briefcase-medical"></i></div>
+                        <div class="card-body">
+                            <h5 class="text-center card-title h3">Prontuário E.</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="container" style="padding-bottom: 5vh">
+        <div class="row">
+            <div class="pb-5 col d-flex justify-content-center">
+                <a href="" class="icon-link">
+                    <div class="card" style="width: 18rem;">
+                        <div class="text-center" style="font-size: 100px"><i class="fas fa-chart-pie"></i></div>
+                        <div class="card-body">
+                            <h5 class="text-center card-title h3">Relatório</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="pb-5 col d-flex justify-content-center">
+                <a href="" class="icon-link">
+                    <div class="card" style="width: 18rem;">
+                        <div class="text-center" style="font-size: 100px"><i class="fas fa-dollar-sign"></i></div>
+                        <div class="card-body">
+                            <h5 class="text-center card-title h3">Recebimento</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="pb-5 col d-flex justify-content-center">
+                <a href="" class="icon-link">
+                    <div class="card" style="width: 18rem;">
+                        <div class="text-center" style="font-size: 100px"><i class="fas fa-file-code"></i></div>
+                        <div class="card-body">
+                            <h5 class="text-center card-title h3">XML</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Include Footer -->
+    <?php include('./import/footer.php'); ?>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-a11y="true"></script>
+
+</body>
+
+</html>
